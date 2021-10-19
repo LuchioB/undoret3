ce1682884a033e0190afe8fd818a51bee192dbcf
unknown <labaquero70@misena.edu.co>
2021-10-18 20:28:15 -0500

    first commit

---
+++ Reto3/src/main/java/Domingo_Reto3/Reto3/RepositorioOrtopedic.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domingo_Reto3.Reto3;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class RepositorioOrtopedic {
+    
+    @Autowired
+    private InterfaceOrtopedic crud;
+
+    public List<Ortopedic> getAll(){
+        return (List<Ortopedic>) crud.findAll();
+    }
+
+    public Optional<Ortopedic> getOrtopedic(int id){
+        return crud.findById(id);
+    }
+
+    public Ortopedic save(Ortopedic ortopedic){
+        return crud.save(ortopedic);
+    }
+    public void delete(Ortopedic ortopedic){
+        crud.delete(ortopedic);
+    }
+    
+}
